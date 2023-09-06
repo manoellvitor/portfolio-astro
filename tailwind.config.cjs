44dcec55fb2246c96e5b66e0da8714a83ba80488
Manoel Lopes <mlopes@seatssoftware.com>
2023-09-06 17:48:27 +0100

    Done with the About section ✔️

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				ocra: ["OCRAbyBT"],
+				courier: ["Courier"]
 			  },
 			animation: {
 			'ping-fast': 'ping 0.7s cubic-bezier(1, 0, 0.2, 1) infinite',
