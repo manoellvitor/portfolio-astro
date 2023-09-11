441ef2c85d6da8adde0d6ff1b21a0ecf66363c6f
Manoel Lopes <mlopes@seatssoftware.com>
2023-09-11 10:40:15 +0100

    Added some real projects to the page. ➕

---
+++ src/projects_data/Projects.ts
@@ -0,0 +1,34 @@
+const projects = [
+    {
+      name: "Trainer",
+      description: "This website is designed to help personal trainers connect with their clients.",
+      image: "",
+      stack: [
+        {
+          name: "React",
+          icon: "BiLogoReact"
+        },
+        {
+          name: "Tailwind CSS",
+          icon: "BiLogoTailwindCss"
+        },
+        {
+          name: "TypeScript",
+          icon: "BiLogoTypescript"
+        }
+      ],
+      urls: [
+        {
+          type: "Live Demo",
+          url: "https://personal-trainer-mu.vercel.app"
+        },
+        {
+          type: "Git Repository",
+          url: "https://github.com/manoellvitor/personal-trainer"
+        }
+      ]
+    },
+  ];
+  
+  export default projects;
+  
