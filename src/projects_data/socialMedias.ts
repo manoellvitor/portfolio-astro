b7ee4952d5168ec02f73b0c971f71490469da453
Manoel Vitor <manoelvitorka@gmail.com>
2024-06-15 12:57:46 +0100

    feat: add new CV

--- src/projects_data/socialMedias.ts
+++ src/projects_data/socialMedias.ts
@@ -1,25 +1,25 @@
 const socialMedias = [
-    {
-        name: "GitHub",
-        url: "https://github.com/manoellvitor",
-        icon: "mdi:github",
-        color: "red",
-        ariaLabel: "Check more on Manoel`s GitHub page."
-    },{
-        name: "Linkedin",
-        url: "https://www.linkedin.com/in/manoellvitor",
-        icon: "mdi:linkedin",
-        color: "green",
-        ariaLabel: "Check more on Manoel`s Linkedin page."
-    },{
-        name: "Resume CV",
-        url: "../../Manoel Lopes - Resume.pdf",
-        icon: "mdi:file-pdf-box",
-        color: "blue",
-        ariaLabel: "Download Manoel`s Resume."
-    }
-    
-]
-
+  {
+    name: "GitHub",
+    url: "https://github.com/manoellvitor",
+    icon: "mdi:github",
+    color: "red",
+    ariaLabel: "Check more on Manoel`s GitHub page.",
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/manoellvitor",
+    icon: "mdi:linkedin",
+    color: "green",
+    ariaLabel: "Check more on Manoel`s Linkedin page.",
+  },
+  {
+    name: "Resume CV",
+    url: "../../manoel-lopes-cv.pdf",
+    icon: "mdi:file-pdf-box",
+    color: "blue",
+    ariaLabel: "Download Manoel`s Resume.",
+  },
+];
 
 export default socialMedias;
